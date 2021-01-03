cced3f3d6ee6cdde3489e9dd9b7a5ebce4840454
anupmjain16 <jainanupam16@gmail.com>
2021-01-03 16:54:42 +0530

    instagram, youtube ,syllabus added . footer change. insta api .

--- js/main.js
+++ js/main.js
@@ -46,15 +46,43 @@ $(function() {
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     });
-      	
-	
-
-
-
+ 
+    
 });
 
+ 
+       /* ==========================================================================
+     For Insta feed
+     ========================================================================== */ 
 
+    //  $(document).ready(function() {
+ 
+    //     // The Url from your Authorisations list  
+    //     var instantTokenApiUrl = 'https://ig.instant-tokens.com/users/fca1c692-1c4e-49fd-b365-1ea6dd8b0240/instagram/17841400516870478/token?userSecret=inwlwy8uu96vztiywejx4'
+        
+    //       $.ajax({
+    //             url: instantTokenApiUrl,
+    //         dataType: 'json'
+    //           })
+    //            .done(function( response ) {
+                
+    //             if (!response.Token) {
+    //               console.log('Error :: ', response);
+    //             } else {
+    //                 let feed = new Instafeed({
+    //                            accessToken: response.Token
+    //                           });
+    //                 feed.run();
+    //               }
+    //     });
+       
+    //   });
 
-
-
-
+    // $(document).ready(function() {
+    //     var feed = new Instafeed({
+    //       accessToken: InstagramToken
+    //     });
+    //     feed.run();
+    //   });
+  
+   
